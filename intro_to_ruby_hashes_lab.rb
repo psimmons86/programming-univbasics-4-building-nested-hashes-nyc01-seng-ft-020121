3a64a0f7d081aabe6eae09e5d4f31ea0835222d3
patrick simmons <hadroncollides@gmail.com>
2021-01-18 21:57:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,11 +14,10 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
  
-  base_hash = {
-    :railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},:names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}
-      
-    }
+     base_hash = {
+    :railroads => {:pieces => 4, :rent_in_dollars => {},:names => {}}
   }
+
 end
 
 def monopoly_with_third_tier
